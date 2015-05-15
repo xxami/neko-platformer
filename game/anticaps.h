17dd70b6cd93c5d515ea89760272306f4965222f
ami <ami@lilah.cat>
2015-05-15 21:07:17 +0100

    readd supported platform macros

--- game/anticaps.h
+++ game/anticaps.h
@@ -2,6 +2,17 @@
 #ifndef _NEKO_ANTICAPS_H_
 #define _NEKO_ANTICAPS_H_
 
+/**
+ * CC_PLATFORM* > cc_platform*
+ */
+#define cc_platform_win32 CC_PLATFORM_WIN32
+#define cc_platform_linux CC_PLATFORM_LINUX
+#define cc_platform_mac CC_PLATFORM_MAC
+#define cc_target_platform CC_TARGET_PLATFORM
+
+/**
+ * CC_LOG* > cc_log*
+ */
 #if (!defined(COCOS2D_DEBUG) || COCOS2D_DEBUG == 0)
 	#define cc_log(...) do {} while (0)
 	#define cc_loginfo(...) do {} while (0)
@@ -20,5 +31,39 @@
 	#define cc_logerror(format,...) cocos2d::log(format, ##__VA_ARGS__)
 	#define cc_logwarn(...) __CCLOGWITHFUNCTION(__VA_ARGS__)
 #endif // COCOS2D_DEBUG
+
+/**
+ * CC_CALLBACK* > cc_callback*
+ */
+#define cc_callback0(__selector__,__target__, ...) \
+	std::bind(&__selector__,__target__, ##__VA_ARGS__)
+#define cc_callback1(__selector__,__target__, ...) \
+	std::bind(&__selector__,__target__, std::placeholders::_1, ##__VA_ARGS__)
+#define cc_callback2(__selector__,__target__, ...) \
+	std::bind(&__selector__,__target__, std::placeholders::_1, \
+	std::placeholders::_2, ##__VA_ARGS__)
+#define cc_callback3(__selector__,__target__, ...) \
+	std::bind(&__selector__,__target__, std::placeholders::_1, \
+	std::placeholders::_2, std::placeholders::_3, ##__VA_ARGS__)
+
+/**
+ * CREATE_FUNC > cc_createfunc
+ */
+#define cc_createfunc(__TYPE__) \
+static __TYPE__* create() \
+{ \
+    __TYPE__ *pRet = new(std::nothrow) __TYPE__(); \
+    if (pRet && pRet->init()) \
+    { \
+        pRet->autorelease(); \
+        return pRet; \
+    } \
+    else \
+    { \
+        delete pRet; \
+        pRet = NULL; \
+        return NULL; \
+    } \
+}
 
 #endif // _NEKO_ANTICAPS_H
--- game/neko.h
+++ game/neko.h
@@ -4,4 +4,12 @@
 
 #include "anticaps.h"
 
+#if (cc_target_platform == cc_platform_win32)
+    #define cc_platform_desktop 1
+#elif (cc_target_platform == cc_platform_linux)
+    #define cc_platform_desktop 1
+#elif (cc_target_platform == cc_platform_mac)
+    #define cc_platform_desktop 1
+#endif
+
 #endif // _NEKO_NEKO_H_
