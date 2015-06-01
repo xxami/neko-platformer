08dc467c4bafbc9fc457ab4f1c49b0ee01b36902
ami <ami@lilah.cat>
2015-06-01 21:51:52 +0100

    added some physics definition

--- game/neko_physics.h
+++ game/neko_physics.h
@@ -18,7 +18,32 @@ namespace neko {
 		typedef PhysicsObject self;
 		typedef cc::Node super;
 
+		/**
+		 * objects this object should collide with
+		 */
+		std::vector<PhysicsObject*> collide_objects;
+
 		public:
+
+		/**
+		 * set object as static or dynamic
+		 */
+		void setStatic(bool is_static);
+
+		/**
+		 * add objects this object should collide with
+		 */
+		void collideWith(int first, ...);
+
+		/**
+		 * set the gravity for this object
+		 */
+		void setGravity(int x, int y);
+
+		/**
+		 * set the velocity for this object
+		 */
+		void setVelocity(int x, int y);
 
 		virtual bool init();
 
@@ -36,7 +61,37 @@ namespace neko {
 		typedef Physics self;
 		typedef cc::Node super;
 
+		/**
+		 * actively simulated
+		 */
+		std::unordered_map<int, PhysicsObject*> dynamic_objects;
+
+		/**
+		 * objects that can affect actively simulated objects
+		 */
+		std::unordered_map<int, PhysicsObject*>  static_objects;
+
 		public:
+
+		/**
+		 * return PhysicsObject by object id
+		 */
+		PhysicsObject *getPhysicsObjectById(int id);
+
+		/**
+		 * simulate physics for all objects in this universe
+		 */
+		void simulate(float delta);
+
+		/**
+		 * add physics object to this universe
+		 */
+		void addObject(int object_id, PhysicsObject *pobj);
+
+		/**
+		 * stop simulating physics for this object
+		 */
+		void removeObject(int object_id);
 
 		virtual bool init();
 
